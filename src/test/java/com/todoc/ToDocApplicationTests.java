d0ab6455e6ba17d7bf1708cced268281d4feed29
jainefer <jy5849@naver.com>
2026-06-13 15:46:46 +0900

    스테이징 제거

--- src/test/java/com/todoc/ToDocApplicationTests.java
+++ src/test/java/com/todoc/ToDocApplicationTests.java
@@ -1,12 +1,36 @@
 package com.todoc;
 
+import com.todoc.repository.FormQuestionRepository;
+import com.todoc.repository.FormSectionRepository;
+import com.todoc.repository.FormTemplateRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ToDocApplicationTests {
 
+    @Autowired
+    private FormTemplateRepository templateRepository;
+
+    @Autowired
+    private FormSectionRepository sectionRepository;
+
+    @Autowired
+    private FormQuestionRepository questionRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void seedsBuildingPermitTemplate() {
+        var template = templateRepository.findByTemplateCode("building_major_repair_use_change_permit");
+
+        assertThat(template).isPresent();
+        assertThat(sectionRepository.findAllByTemplateIdOrderByOrderNo(template.get().getId())).isNotEmpty();
+        assertThat(questionRepository.count()).isGreaterThan(0);
     }
 }
